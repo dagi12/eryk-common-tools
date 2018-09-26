1ef77c9e54848a5da7a53d66fcd4bdb5c1598f5d
Eryk Mariankowski <dagi12@o2.pl>
2018-09-26 18:34:32 +0200

    change extension in directory

--- common-scripts.sh
+++ common-scripts.sh
@@ -14,3 +14,15 @@ done
 
 # rename fix recursively
 find . -name "*.java" -exec rename 's/\.java$/.kt/' '{}' \;
+
+# change extension of all
+for file in *.txt; do
+    mv "$file" "$(basename "$file" .txt).md"
+done
+
+# change git extension
+find . -name "*.scss" -delete
+for i in $(find . -iname "*.scss"); do git mv "$i" "$(echo $i | rev | cut -d '.' -f 2- | rev).css"; done
+
+# current working directory
+echo $PWD
