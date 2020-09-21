e36cf8ee2c9f5014cf1b4670fc7a6ecb35d8733d
Eryk Mariankowski <dagi12@o2.pl>
2020-09-21 14:30:42 +0200

    submodule update

--- common-scripts.sh
+++ common-scripts.sh
@@ -15,10 +15,20 @@ done
 # rename fix recursively
 find . -name "*.java" -exec rename 's/\.java$/.kt/' '{}' \;
 
+# change encoding recursively
+find . -type f -print -exec iconv -f windows-1250 -t utf-8 -o {}.converted {} \; -exec mv {}.converted {} \;
+
 # change extension of all
 for file in *.txt; do
     mv "$file" "$(basename "$file" .txt).md"
 done
+
+# convert to ts
+for file in *.js; do
+    react-js-to-ts "$file"
+done
+
+for f in $(find . -name '*.js'); do react-js-to-ts $f; done
 
 # change git extension
 find . -name "*.scss" -delete
